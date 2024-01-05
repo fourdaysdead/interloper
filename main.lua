649de074ff9ed4778a0fdd3e9503fd926ff1fa2a
brie <151115640+idkbrie@users.noreply.github.com>
2024-01-05 19:43:11 -0300

    bwomp

--- main.lua
+++ main.lua
@@ -1,10 +1,2 @@
 Interloper = RegisterMod("Interloper", 1)
 local mod = Interloper
-
-function Coolio:(_,_,_,player)
-
-    IIsaac.Spawn:(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, 0, Vector(0,0), Vector(0,0), player)
-
-end
-
-mod:AddCallback.ModCallbacks(MC_POST_PLAYER_INIT, Coolio)
