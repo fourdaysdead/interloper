a0b2bb38477047b88391e80b368c14dee941fa57
fourdaysdead <brie4dd@gmail.com>
2026-08-09 02:53:31 -0300

    Move to main.lua + Neptunus + Character tweaks

--- main.lua
+++ main.lua
@@ -1,10 +1,24 @@
 Interloper = RegisterMod("Interloper", 1)
 local mod = Interloper
 
-local grate = include("scripts/characters/grate")
-
-grate:CheckIfGrate()
-grate:RemoveCostume()
 
 
+local gratePlayer = Isaac.GetPlayerTypeByName("Grate")
 
+local function CheckIfGrate(player)
+    if player:GetPlayerType() == gratePlayer then
+        return true
+    else 
+        return false
+    end
+end
+
+local function GiveNeptunus(_,player)
+    if CheckIfGrate(player) == true then
+        player:AddInnateCollectible(CollectibleType.COLLECTIBLE_NEPTUNUS, 1, "forevah", -1, false )
+    else
+        return
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, GiveNeptunus)
